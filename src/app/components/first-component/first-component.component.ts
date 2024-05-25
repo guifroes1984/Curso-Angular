90d86899f7417699f79a04452134400e21b852a3
guifroes1984 <guilhermefroes26@gmail.com>
2024-05-25 01:23:37 -0300

    Dados no template.

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponentComponent implements OnInit {
 
+  name: string = 'Guilherme';
+  age: number = 39;
+  job = 'Programador';
+  hobbies = ['vôlei', 'correr', 'estudar'];
+  car = {
+    name: 'Gol G3',
+    year: 2003,
+  }
+
   constructor() { }
 
   ngOnInit(): void {
